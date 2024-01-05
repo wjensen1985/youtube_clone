8e6767c6c524814d2bdd4494ade5519586f878d7
William <william.hw.jensen@gmail.com>
2024-01-04 20:02:57 -0500

    added make-thumbnail post request to video processing service

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -14,7 +14,7 @@ app.post("/process-video", (req, res) => {
     }
 
     ffmpeg(inputFilePath)
-        .outputOptions('-vf', 'scale=0:360') // 360p
+        .outputOptions(['-vf', 'scale=360:-1']) // 360p
         .on('end', function() {
             console.log('Processing finished successfully');
             res.status(200).send('Processing finished successfully');
@@ -24,6 +24,30 @@ app.post("/process-video", (req, res) => {
             res.status(500).send('An error occurred: ' + err.message);
         })
         .save(outputFilePath);
+});
+
+app.post("/make-thumbnail", (req, res) => {
+    // get path of the input video file from request body
+    const videoFilePath = req.body.videoFilePath;
+    const thumbnailFilePath = req.body.thumbnailFilePath;
+
+    if (!videoFilePath || !thumbnailFilePath){
+        res.status(400).send("bad request: missing file path.");
+    }
+
+    ffmpeg(videoFilePath)
+        .takeScreenshots({
+            filename: thumbnailFilePath,
+            count: 1
+        })
+        .on('end', function() {
+            console.log('Processing finished successfully');
+            res.status(200).send('Processing finished successfully');
+        })
+        .on('error', function(err: any) {
+            console.log('An error occurred: ' + err.message);
+            res.status(500).send('An error occurred: ' + err.message);
+        })
 });
 
 const port = process.env.PORT || 3000;
