09b7e13f9395e9d6d58adc7065dfd32c86da08cd
William <william.hw.jensen@gmail.com>
2024-01-14 14:15:55 -0500

    updating yt-web-client to add display thumbnails and deployed client

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -5,18 +5,21 @@ import Image from 'next/image';
 
 export default async function Home() {
   const videos = await getVideos();
-  // const thumbnailPrefix = 'https://storage.googleapis.com/whwj-yt-clone-thumbnails/';
+  const thumbnailPrefix = 'https://storage.googleapis.com/whwj-yt-clone-thumbnails/';
+  // {/* <h3>{thumbnailPrefix + video.thumbnailFilename}</h3> */}
 
   return (
     <main className={styles.main}>
       {
         videos.map((video) => (
-          <Link href={`watch?v=${video.filename}`}>
-            <Image src="" alt='video' width={120} height={80}
-              className={styles.thumbnail}/>
-          </Link>
+            <Link key={`${video.id}`} href={`watch?v=${video.filename}`}>
+              <Image src={thumbnailPrefix + video.thumbnailFilename} alt='video' width={120} height={80}
+                className={styles.thumbnail}/>
+            </Link>
         ))
       }
     </main>
   )
 }
+
+export const revalidate = 30;
